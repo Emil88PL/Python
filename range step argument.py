41d95d7e0cd23370e9e2bea035db85e6e7e9d266
Emil <emil88pl@gmail.com>
2018-01-24 19:59:36 +0000

    Add files via upload

---
+++ range step argument.py
@@ -0,0 +1,8 @@
+print('My name is')
+for i in range(0,16, 2):  # step argument
+    print('Jim name times ' + str(i))
+
+
+print('My name is')
+for i in range(10,-1, -1):  # step argument
+    print('Jim name times ' + str(i))
---
+++ range.py
@@ -0,0 +1,3 @@
+print('My name is')
+for i in range(12,16):
+    print('Jim name times ' + str(i))
