461bd0df7072a24027f8d0dd50027cf09de7009a
Emil <emil88pl@gmail.com>
2018-02-25 20:28:23 +0000

    Add files via upload

---
+++ List.py
@@ -0,0 +1,90 @@
+# Index = Single value
+# Slice - List of values
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+
+'The ' + spam[-1] + ' is afraid of the ' + spam[2] + '.'
+
+###
+
+
+spam[1:3]
+
+
+
+###
+
+
+spam = [10,20,30]
+spam[1] = 'hello'
+
+# spam = [10, 'hello', 30]
+
+spam[1:3] = ['cat', 'dog', 'mouse']
+
+# spam = [10, 'cat', 'dog', 'mouse']
+
+
+### Slice Shortcuts
+
+spam = ['cat', 'bat', 'rat', 'elephant']
+
+spam[:2]
+
+# spam = ['cat','bat']
+
+spam[1:]
+
+# spam = ['bat', 'rat', 'elephant']
+
+### del Statment
+
+del spam[2]
+
+### String and list similarities
+
+len['hello')
+5
+
+
+len([1,2,3])
+3
+
+
+
+[1,2,3] + [4,5,6]
+
+[1,2,3,4,5,6]
+
+[1,2,3] * 2
+
+[1,2,3,1,2,3]
+
+
+### List function
+list('hello')
+['h','e','l','l','o']
+
+### The in and not in operator
+
+'howdy' in ['hello', 'hi', 'howdy', 'heyah']
+TRUE
+42 in ['hello', 'hi', 'howdy', 'heyah']
+FALSE
+
+'howdy' not in ['hello', 'hi', 'howdy', 'heyah']
+FALSE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
