6325b8ce40f224cce5d27b22b42a628c5aa5d81a
Emil <emil88pl@gmail.com>
2018-05-23 18:23:27 +0100

    Add files via upload

---
+++ Count letters.py
@@ -0,0 +1,13 @@
+import pprint
+
+message = 'It was a bright day in April, and the clock were striking thirteen.'
+count = {}
+
+for character in message.upper():
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
+
+
+### triple '''
