8b640fdba7a2f767bbec695d5e84b98479e68a4f
Emil <emil88pl@gmail.com>
2018-05-27 17:28:47 +0100

    Add files via upload

---
+++ Data structures.py
@@ -0,0 +1,19 @@
+
+cat = {'name': 'Zophie', 'age': 7, 'color': 'gray'}
+allCats = []
+
+allcats.append({'name': 'Zophie', 'age': 7, 'color': 'gray'})
+allcats.append({'name': 'Pooka', 'age': 8, 'color': 'black'})
+allcats.append({'name': 'Fat-tail', 'age': 5, 'color': 'white'})
+allcats.append({'name': '????', 'age': 4, 'color': 'orange'})
+
+###
+
+type(42)
+<class 'int'>
+type('hello')
+<class 'string'>
+type(3.14)
+<class 'float'>
+type(theBoard)
+<class 'dict'>
